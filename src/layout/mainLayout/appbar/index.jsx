fabee17edf53264b3855c4add34bad13d82ab7fe
rohan kachhore <rohankachhore16@gmail.com>
2023-06-21 16:05:23 +0530

    add project setup

---
+++ src/layout/mainLayout/appbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Appbar = () => {
+  return (
+    <>
+    <h1>appbar</h1>
+    </>
+  )
+}
+
+export default Appbar
---
+++ src/layout/mainLayout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainLayout = () => {
+  return (
+    <>
+      <Box>
+          
+      </Box>
+    </>
+  )
+}
+
+export default MainLayout
